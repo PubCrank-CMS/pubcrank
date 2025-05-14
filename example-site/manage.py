a94fd81cc806b830d09d1adb996f6debe3546c98
Paul Bailey <paul@neutron.studio>
2025-05-14 14:59:51 -0500

    setup

--- example-site/manage.py
+++ example-site/manage.py
@@ -2,11 +2,13 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from pathlib import Path
 
+sys.path.append(str(Path(__file__).parent.parent / 'pubcrank' / 'src'))
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pubex.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pubsite.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
